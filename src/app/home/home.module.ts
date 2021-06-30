c0208a8f15fb6b73513b7b0be777a922fd0655ab
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-06-30 17:26:56 -0300

    creating new component novo-usuario

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,12 +6,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NovoUsuarioComponent
   ],
   imports: [
     CommonModule,
