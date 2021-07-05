996de6d3503334f4aefee6c2aad4873405fbc9bc
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-07-05 17:42:18 -0300

    New user Autentication and password

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoUsuarioComponent implements OnInit {
 
-  constructor() { }
+  novoUsuarioFrom!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ) { }
 
   ngOnInit(): void {
+    this.novoUsuarioFrom = this.formBuilder.group({
+      userName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
+      password: [''],
+    });
+  }
+
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioFrom.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
   }
 
 }
