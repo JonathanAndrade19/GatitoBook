6a9b8ea484cc15ccf6785d25267438f8ee39e5da
JonathanAndrade19 <jonathanjampamb@gmail.com>
2021-07-06 17:12:28 -0300

    creating new service usuario-existe

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { NovoUsuario } from './novo-usuario';
+import { UsuarioExisteService } from './usuario-existe.service';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { minusculoValidator } from './minusculo.validator';
 import { tamanhoSenha } from './senha.validator';
+import { NovoUsuario } from './novo-usuario';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -17,12 +18,13 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExisteService: UsuarioExisteService 
   ) { }
 
   ngOnInit(): void {
     this.novoUsuarioFrom = this.formBuilder.group({
-      userName: ['', [minusculoValidator]],
+      userName: ['', [minusculoValidator], [this.usuarioExisteService.usuarioJaExite()]],
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       password: ['', [tamanhoSenha]],
