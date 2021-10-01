52f19ee5e2332248b33309be6cae81494817a5f1
JonathanAndrade <jonathanjampamb@gmail.com>
2021-09-30 21:25:30 -0300

    [CREATE] #detalhe-animal criando a funçao de curtir e deletar img

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,12 +1,13 @@
 import { TokenService } from './../autenticacao/token.service';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Injectable } from '@angular/core';
 import { Animais, Animal } from './animais';
+import { catchError, mapTo } from 'rxjs/operators';
 
 const API = environment.apiURL;
-
+const NOT_MODIFIED = '304';
 @Injectable({
   providedIn: 'root'
 })
@@ -20,6 +21,21 @@ export class AnimaisService {
 
   buscarPorId(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluirAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.http
+      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
+      .pipe(
+        mapTo(true),
+        catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      );
   }
 
 }
